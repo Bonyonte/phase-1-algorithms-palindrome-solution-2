f9784c909ead0b2e2c5b06038b71191545ae49a2
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-17 17:28:20 +0300

    commit tree e31596e4e63a2f44d1d951116b6f8a1879cf4438

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+function reverseString(word){
+
+  return word.split("").reverse().join("");
+ }
 function isPalindrome(word) {
+  const reverseWord = reverseString(word);
+
+  return word === reverseWord;
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+
+  if the word is the same as the one in reverse, then return true
+reverse the input string
+if the input string is same as the reverse input
+
+return true
+
+else
+
+return false
 */
 
 /*
   Add written explanation of your solution here
+
+   Use split method so the string turns into an array with each letter.
+  Now we can easily iterate over it with an array method
+  such as .reverse. The goal is to see if the string is the same
+  forwards and backwards. Then join the new flipped string, 
+  and compare it with the orginal string to return true or false.
 */
 
 // You can run `node index.js` to view these console logs
